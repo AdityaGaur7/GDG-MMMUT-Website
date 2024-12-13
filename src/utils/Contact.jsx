0457a3cbd87db145aba60f70e12ad1f4c167cd7d
A-P-Singh-27 <adityaaditya50128@gmail.com>
2024-12-14 01:22:36 +0530

    Update imports

--- src/utils/Contact.jsx
+++ src/utils/Contact.jsx
@@ -3,7 +3,8 @@ import {
     Dialog,
     DialogContent,
     DialogTrigger,
-} from "@/components/ui/dialog.jsx"
+ } from '@/components/ui/dialog';
+
 
 function Contact() {
     return (
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src', 
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
